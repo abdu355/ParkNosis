e564904ba506da913b3fe35b6872df9dbb024a02
abdu355 <abdu_sah@hotmail.com>
2015-11-20 13:44:25 +0400

    v1.0

--- app/src/main/java/com/example/b00047562/parkinson_mhealth/Accelerometer.java
+++ app/src/main/java/com/example/b00047562/parkinson_mhealth/Accelerometer.java
@@ -23,7 +23,7 @@ public class Accelerometer extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });//hi
     }
 
 }
