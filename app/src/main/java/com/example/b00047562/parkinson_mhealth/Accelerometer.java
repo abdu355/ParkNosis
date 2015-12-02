bbb86dba7d28d9388bc7dc6320931845f8898a51
abdu355 <abdu_sah@hotmail.com>
2015-12-02 19:51:40 +0400

    v1.0 Added Shake threshold detection

--- app/src/main/java/com/example/b00047562/parkinson_mhealth/Accelerometer.java
+++ app/src/main/java/com/example/b00047562/parkinson_mhealth/Accelerometer.java
@@ -1,11 +1,13 @@
 package com.example.b00047562.parkinson_mhealth;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,15 +17,28 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.b00047562.parkinson_mhealth.R;
 
 public class Accelerometer extends AppCompatActivity implements SensorEventListener{
 
-    private TextView txtXValue, txtYValue, txtZValue;
+    private TextView txtXValue, txtYValue, txtZValue,tv_shakeAlert;
     private SensorManager MySensorManager;
     private Sensor MyAclmeter;
-    private float ax , ay, az;
+    private float ax , ay, az,lastx,lasty,lastz;
+    private long lastUpdate;
+    private static final int SHAKE_THRESHOLD = 1700;
+    private static boolean output_upToDate = true;
+
+    /* Handles the refresh */
+    private Handler outputUpdater = new Handler();
+
+    /* Adjust this value for your purpose */
+    public static final long REFRESH_INTERVAL = 100;      // in milliseconds
+
+    /* This object is used as a lock to avoid data loss in the last refresh */
+    private static final Object lock = new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +51,7 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         txtXValue = (TextView) findViewById(R.id.txtXValue);
         txtYValue = (TextView) findViewById(R.id.txtYValue);
         txtZValue = (TextView) findViewById(R.id.txtZValue);
+        tv_shakeAlert=(TextView)findViewById(R.id.tv_shake);
 
         //Get SensorManager and accelerometer
         MySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -46,6 +62,27 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         else {
             Log.d("Accelerometer not found", "Accelerometer not found");
         }
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(3000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateTextView();
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+
     }
 
     @Override
@@ -73,14 +110,33 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     @Override
     public void onSensorChanged(SensorEvent event) {
         try {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                //Get accelerometer values
-                ax = (float)event.values[0];
-                ay = (float)event.values[1];
-                az = (float)event.values[2];
-                String x = String.format("%2f",ax);
-                String y = String.format("%2f",ay);
-                String z = String.format("%2f",az);
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+               // outputUpdater.post(outputUpdaterTask);
+                long curTime = System.currentTimeMillis();
+                if ((curTime - lastUpdate) > 100) {
+                    long diffTime = (curTime - lastUpdate);
+                    lastUpdate = curTime;
+
+                    //Get accelerometer values
+                    ax = (float) event.values[0];
+                    ay = (float) event.values[1];
+                    az = (float) event.values[2];
+                    float speed = Math.abs(ax + ay + az - lastx - lasty - lastz) / diffTime * 10000;
+
+                    if (speed > SHAKE_THRESHOLD) {
+                        //Log.d("sensor", "shake detected w/ speed: " + speed);
+                        //Toast.makeText(this, "shake detected w/ speed: " + speed, Toast.LENGTH_SHORT).show();
+                        tv_shakeAlert.setText("Hold Steady !");
+                        tv_shakeAlert.setTextColor(Color.RED);
+                    }
+
+                lastx = ax;
+                lasty = ay;
+                lastz = az;
+                }
+                String x = String.format("%2f", ax);
+                String y = String.format("%2f", ay);
+                String z = String.format("%2f", az);
                 //change display values
                 txtXValue.setText(x);
                 txtYValue.setText(y);
@@ -95,4 +151,11 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //Do something
     }
+
+    private void updateTextView() {
+        tv_shakeAlert.setText("Steady");
+        tv_shakeAlert.setTextColor(Color.GREEN);
+    }
+
+
 }
