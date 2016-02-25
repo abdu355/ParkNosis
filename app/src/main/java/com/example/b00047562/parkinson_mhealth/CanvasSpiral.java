2b27edda84154c1e256f9d1bd2c1c1d6318b78dd
OSALM <b00044877@gmail.com>
2016-02-25 04:17:51 +0400

    Spiral Test updated

--- app/src/main/java/com/example/b00047562/parkinson_mhealth/CanvasSpiral.java
+++ app/src/main/java/com/example/b00047562/parkinson_mhealth/CanvasSpiral.java
@@ -1,57 +1,89 @@
 package com.example.b00047562.parkinson_mhealth;
 
+/**
+ * Created by OS on 2/22/2016.
+ */
+
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
-import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 
-//kaka
-public class DrawView extends View {
-    //drawing path
+public class CanvasSpiral extends View {
+
     private Path drawPath;
     //drawing and canvas paint
     private Paint drawPaint, canvasPaint;
     //initial color
-    private int paintColor = 0xFF660000;
+  //  private int paintColor = 0xFF660000;
     //canvas
     private Canvas drawCanvas;
     //canvas bitmap
     private Bitmap canvasBitmap;
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    Context context;
+    private Paint mPaint,mPaint2;
+    int maxX,maxY;
 
 
     //usable by SpiralRedraw
     public static ArrayList<SpiralData> spiralData;
 
+    public CanvasSpiral(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        this.context = context;
+        // and we set a new Paint with the desired attributes
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(10f);
+        mPaint.setAlpha(0x80);
 
-    public DrawView(Context context, AttributeSet attrs){
-        super(context, attrs);
+        mPaint2= new Paint();
+        mPaint2.setAntiAlias(true);
+        mPaint2.setColor(Color.GREEN);
+        mPaint2.setStyle(Paint.Style.STROKE);
+        mPaint2.setStrokeJoin(Paint.Join.ROUND);
+        mPaint2.setStrokeWidth(10f);
+
+        Display mdisp =((Activity) context).getWindowManager().getDefaultDisplay();
+        Point mdispSize = new Point();
+        mdisp.getSize(mdispSize);
+        maxX = mdispSize.x;
+        maxY = mdispSize.y;
+
         setupDrawing();
     }
+
+
     private void setupDrawing(){
 //get drawing area setup for interaction
         drawPath = new Path();
         drawPaint = new Paint();
 
-        drawPaint.setColor(paintColor);
+      //  drawPaint.setColor(paintColor);
 
         drawPaint.setAntiAlias(true);
         drawPaint.setStrokeWidth(10);
         drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaint.setColor(Color.GREEN);
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
 
@@ -59,19 +91,57 @@ public class DrawView extends View {
         spiralData = new ArrayList<>();
 
     }
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-//view given size
         super.onSizeChanged(w, h, oldw, oldh);
+
+        // your Canvas will draw onto the defined Bitmap
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         drawCanvas = new Canvas(canvasBitmap);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-//draw view
+        super.onDraw(canvas);
+
+        float angle,x ,y ;
+        float []pointsArray = new float[2160];
+        float []Moyes = new float[168];
+
+        int j=0;
+        for (int i=0; i< 1080; i++,j=j+2) {
+            angle = 0.50502f * i;
+
+            x = maxX / 2 -10+ (5 + angle) * (float) Math.cos(angle);
+            y = (maxY / 2)-250 + (5 + angle) * (float) Math.sin(angle);
+
+
+            pointsArray[j]=x;
+            pointsArray[j+1]=y;
+        }
+        int k=0;
+        for (int m=0;m<2160;m++,k=k+2)
+        {
+            Moyes[k]=pointsArray[m];
+            Moyes[k+1]=pointsArray[m+1];
+            m+=25;
+
+        }
+        for (int i=0;i<166;i=i+2)
+        {
+            canvas.drawLine(Moyes[i],Moyes[i+1],Moyes[i+2],Moyes[i+3],mPaint);
+        }
+
+    //    canvas.drawPoints(Moyes,mPaint2);
+
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
         canvas.drawPath(drawPath, drawPaint);
+
     }
 
     @Override
@@ -88,16 +158,16 @@ public class DrawView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 drawPath.moveTo(touchX, touchY);
-                Spiral.alert.setTextColor(Color.parseColor("#00e676"));
-                Spiral.alert.setText("OK!");
+//                Spiral.alert.setTextColor(Color.parseColor("#00e676"));
+//                Spiral.alert.setText("OK!");
                 break;
             case MotionEvent.ACTION_MOVE:
                 drawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
                 drawCanvas.drawPath(drawPath, drawPaint);
-                Spiral.alert.setTextColor(Color.RED);
-                Spiral.alert.setText("KEEP TOUCHING!");
+//                Spiral.alert.setTextColor(Color.RED);
+//                Spiral.alert.setText("KEEP TOUCHING!");
                 drawPath.reset();
                 Spiral.redrawOpen.setEnabled(true); // to prevent crashing or redrawing when no data is available - this will updated soon
                 break;
--- app/src/main/java/com/example/b00047562/parkinson_mhealth/Spiral.java
+++ app/src/main/java/com/example/b00047562/parkinson_mhealth/Spiral.java
@@ -1,27 +1,19 @@
 package com.example.b00047562.parkinson_mhealth;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Spiral extends AppCompatActivity implements  View.OnClickListener {
 
-    public static Button redrawopen;
+    public static Button redrawOpen,btnClr;
 
-    private DrawView myview;
+
+    private CanvasSpiral customCanvas;
     public static TextView alert;
 
     @Override
@@ -31,25 +23,25 @@ public class Spiral extends AppCompatActivity implements  View.OnClickListener {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        redrawopen=(Button)findViewById(R.id.btn_clear);
-        myview=(DrawView)findViewById(R.id.drawing);
-        alert=(TextView)findViewById(R.id.tv_alert);
-        redrawopen.setOnClickListener(this);
+        customCanvas=(CanvasSpiral)findViewById(R.id.spiral_canvas);
+        redrawOpen =(Button)findViewById(R.id.btn_redraw);
+        btnClr= (Button) findViewById(R.id.btn_clear);
 
-
+        btnClr.setOnClickListener(this);
+        redrawOpen.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId())
         {
-
             case R.id.btn_clear:
                 //Toast.makeText(this,"clear",Toast.LENGTH_SHORT).show();
-                //myview.cleardisp();
+                customCanvas.cleardisp();
+                break;
+            case R.id.btn_redraw:
                 this.startActivity(new Intent(this, SpiralRedraw.class));
                 break;
-
         }
     }
 }
